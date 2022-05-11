3fe4147fd248777863e3280946ab58098c3a4dc8
adam-25 <adarsh.dudhat11@gmail.com>
2022-05-11 10:33:27 -0600

    Update Error Messages

--- BackEnd/MiddleWare/errors.js
+++ BackEnd/MiddleWare/errors.js
@@ -17,5 +17,24 @@ module.exports = (err, req, res, next) => {
 		err = new ErrorHandler(message, 400);
 	}
 
+	// MongoDB duplicate key error.
+	if (err.code === 11000) {
+		const message = "Duplicate "+ Object.keys(err.keyValue) +" Entered.";
+
+		err = new ErrorHandler(message, 400);
+	}
+
+	// JWT Web Token error.
+	if (err.name === "JsonWebTokenError") {
+		const message = "JsonWebTokenError is invalid. Please try again.";
+		err = new ErrorHandler(message, 400);
+	};
+
+	// JWT Got expired
+	if (err.name === "TokenExpiredError") {
+		const message = "JWT web Token has been expired. Please try again.";
+		err = new ErrorHandler(message, 400);
+	};
+
 	res.status(err.statusCode).json({success: false, message: err.message});
 }
