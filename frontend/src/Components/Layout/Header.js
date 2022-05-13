3fac36e5b58f29cd009a0ba2b8dbfb6431aa2bff
adam-25 <adarsh.dudhat11@gmail.com>
2022-05-13 17:44:36 -0600

    PreNavBar completed

---
+++ frontend/src/Components/Layout/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PreHeader from './PreHeader';
+
+
+const Header = () => {
+	return (
+		<PreHeader />
+	)
+}
+
+export default Header
---
+++ frontend/src/Components/Layout/PreHeader.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import logo from '../../Images/logo.png';
+import '../../Styles/preHeaderStyles.css';
+
+const PreHeader = () => {
+
+	return (
+		<div>
+			<nav className="custom-navbar navbar navbar-expand-lg navbar-dark">
+				<a className="navbar-brand" href="/Home"><img className="logo" src={logo} alt="" /></a>
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<form className="navbar-brand search" action="">
+						<input className="search-field" type="text" placeholder="Search..." name="search" autoComplete='off' />
+						<button className="btn btn-warning search-btn" type="submit"><i className="fa fa-search"></i></button>
+					</form>
+					<ul className="navbar-nav ml-auto">
+						<li className="nav-item">
+							<a className="nav-link" href="/Login"><span className="sign-in"> Sign-In & </span><br/> <span className="register"> Register </span></a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link order" href="/myOrders"><span className='order'><i class="fas fa-shopping-bag"></i>  Orders</span></a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link cart" href="/Cart"><span className='cart'><i className="fas fa-shopping-cart"></i>  Cart </span></a>
+						</li>
+					</ul>
+				</div>
+			</nav >
+		</div>
+	)
+}
+
+export default PreHeader;
