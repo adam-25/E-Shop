d22805d8f50e4f9572b2afb5da8db3c957668cde
adam-25 <adarsh.dudhat11@gmail.com>
2022-05-14 10:53:33 -0600

    Footer Completed

--- frontend/src/Components/Layout/Footer/Footer.js
+++ frontend/src/Components/Layout/Footer/Footer.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import IOSApp from '../../../Images/appStoreDownload.png';
+import androidApp from '../../../Images/playStoreDownload.png';
+import logo from '../../../Images/logo.png';
+import '../../../Styles/footerStyles.css';
 
 const Footer = () => {
 	return (
-		<div>Footer</div>
+
+		<section id="foo">
+			<div className="row">
+				<div className="col-lg-4 col-md-6">
+					<div className="footer">
+						<h3>Download Our App for IOS and Android</h3>
+						<a href="https://www.apple.com/ca/app-store/" target="_blank"><img className="ios" src={IOSApp} alt="Download IOS App" /></a>
+						<a href="https://play.google.com/store" target="_blank"><img className="android" src={androidApp} alt="Download Android App" /></a>
+					</div>
+				</div>
+				<div className="col-lg-4 col-md-6">
+					<div className="footer">
+						<a href="/Home"><img className="logo-footer" src={logo} alt="Web-site logo" /></a>
+						<p>Copyright 2022 &copy; E-Shop</p>
+					</div>
+				</div>
+				<div className="col-lg-4 col-md-12">
+					<div className="footer">
+						<h3>CONTACT US</h3>
+						<a href="https://www.youtube.com/channel/UCqopxIRqpOAa0hpcXP7ngMA" target="_blank"><i class="fab fa-youtube fa-lg icon"></i></a>
+						<a href="https://www.instagram.com/adarsh_11_/" target="_blank"><i class="fab fa-instagram fa-lg icon"></i></a>
+						<a href="https://twitter.com/Adarsh_Dudhat" target="_blank"><i class="fab fa-twitter fa-lg icon"></i></a>
+						<a href="https://www.linkedin.com/in/adarsh-dudhat/" target="_blank"><i class="fab fa-linkedin fa-lg icon"></i></a>
+					</div>
+				</div>
+			</div>
+
+		</section>
 	)
 }
 
