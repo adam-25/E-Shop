86798f32a35006423782a0b1162ac5e7746a03fb
adam-25 <adarsh.dudhat11@gmail.com>
2022-05-14 00:11:14 -0600

    Navbar Completed all the issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
-import Header from './Components/Layout/Header.js';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Header from './Components/Layout/Headers/Header.js';
+import Footer from './Components/Layout/Footer/Footer.js';
+
 function App() {
 	return (
+		<Router>
 			<Header />
+			<Footer />
+		</Router>
 	);
 }
 
--- frontend/src/Components/Layout/Headers/PreHeader.js
+++ frontend/src/Components/Layout/Headers/PreHeader.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import logo from '../../Images/logo.png';
-import '../../Styles/preHeaderStyles.css';
+import logo from '../../../Images/logo.png';
+import '../../../Styles/preHeaderStyles.css';
 
 const PreHeader = () => {
 
 	return (
 		<section className="navBar">
-			<nav className="custom-navbar navbar navbar-expand-lg navbar-dark">
-				<a className="navbar-brand" href="/Home"><img className="logo" src={logo} alt="" /></a>
+			<nav className="navbar navbar-expand-lg navbar-dark">
+				<a className="navbar-brand" href="/Home"><img className="logo" src={logo} alt="Logo" /></a>
+				<a className="nav-link products" href="/Products"> Products </a>
 				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
+				
+
 
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<form className="navbar-brand search" action="">
@@ -18,8 +21,8 @@ const PreHeader = () => {
 						<button className="btn btn-warning search-btn" type="submit"><i className="fa fa-search"></i></button>
 					</form>
 					<ul className="navbar-nav ml-auto">
-						<li className="nav-item">
-							<a className="nav-link" href="/Login"><span className="sign-in"> Sign-In & </span><br/> <span className="register"> Register </span></a>
+						<li className="nav-item sign">
+							<a className="nav-link" href="/Login"><span className="sign-in"> Sign-In & </span><br /> <span className="register"> Register </span></a>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link order" href="/myOrders"><span className='order'><i class="fas fa-shopping-bag"></i>  Orders</span></a>
