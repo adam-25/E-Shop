f17e6c53d57712c9c154f7618609e2985c6ab22a
adam-25 <adarsh.dudhat11@gmail.com>
2022-05-19 15:09:03 -0600

    loading effect

--- frontend/src/Components/User/LoginRegister.js
+++ frontend/src/Components/User/LoginRegister.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import './LoginRegister.css';
 import front from "../../Images/login-front.jpeg";
+import Loading from "../Loading/Loading";
 import { Link, useHistory } from 'react-router-dom';
-import  { Redirect } from 'react-router-dom';
 
 import { toast } from 'react-toastify';
 
@@ -61,90 +61,94 @@ const LoginRegister = () => {
 	}
 
 	return (
-		<div className="login-register">
-			<div class="container">
-				<input type="checkbox" id="flip" />
-				<div class="cover">
-					<div class="front">
-						<img src={front} alt="Login" />
+		<Fragment>
+			{loading ? <Loading /> :
+				<div className="login-register">
+					<div class="container">
+						<input type="checkbox" id="flip" />
+						<div class="cover">
+							<div class="front">
+								<img src={front} alt="Login" />
+							</div>
+						</div>
+						<div class="forms">
+							<div class="form-content">
+								<div class="login-form">
+									<div class="title">Login</div>
+									<form onSubmit={loginSubmit}>
+										<div class="input-boxes">
+											<div class="input-box">
+												<i class="fas fa-envelope"></i>
+												<input type="email"
+													placeholder="Enter your email"
+													required
+													value={loginEmail}
+													onChange={(e) => setLoginEmail(e.target.value)} />
+											</div>
+											<div class="input-box">
+												<i class="fas fa-lock"></i>
+												<input type="password"
+													placeholder="Enter your password"
+													required
+													value={loginPassword}
+													onChange={(e) => setLoginPassword(e.target.value)} />
+											</div>
+											<div class="text">
+												<Link to="/password/forgotPassword">Forgot password?</Link>
+											</div>
+											<div class="button input-box">
+												<input type="submit" value="Log In" />
+											</div>
+											<div class="text sign-up-text">Don't have an account? <label for="flip">Sigup now</label></div>
+										</div>
+									</form>
+								</div>
+								<div class="signup-form">
+									<div class="title">Signup</div>
+									<form onSubmit={registerSubmit}>
+										<div class="input-boxes">
+											<div class="input-box">
+												<i class="fas fa-user"></i>
+												<input type="text"
+													placeholder="Enter your name"
+													required
+													name='name'
+													value={userDetail.name}
+													onChange={changeUserDetail} />
+											</div>
+											<div class="input-box">
+												<i class="fas fa-envelope"></i>
+												<input type="email"
+													placeholder="Enter your email"
+													required
+													name='email'
+													value={userDetail.email}
+													onChange={changeUserDetail} />
+											</div>
+											<div class="input-box">
+												<i class="fas fa-lock"></i>
+												<input type="password"
+													placeholder="Enter your password"
+													required
+													name='password'
+													value={userDetail.password}
+													onChange={changeUserDetail}
+												/>
+											</div>
+											<div class="button input-box">
+												<input type="submit"
+													value="Register" />
+											</div>
+											<div class="text sign-up-text">Already have an account? <label for="flip">Login now</label></div>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div class="forms">
-					<div class="form-content">
-						<div class="login-form">
-							<div class="title">Login</div>
-							<form onSubmit={loginSubmit}>
-								<div class="input-boxes">
-									<div class="input-box">
-										<i class="fas fa-envelope"></i>
-										<input type="email"
-											placeholder="Enter your email"
-											required
-											value={loginEmail}
-											onChange={(e) => setLoginEmail(e.target.value)} />
-									</div>
-									<div class="input-box">
-										<i class="fas fa-lock"></i>
-										<input type="password"
-											placeholder="Enter your password"
-											required
-											value={loginPassword}
-											onChange={(e) => setLoginPassword(e.target.value)} />
-									</div>
-									<div class="text">
-										<Link to="/password/forgotPassword">Forgot password?</Link>
-									</div>
-									<div class="button input-box">
-										<input type="submit" value="Log In" />
-									</div>
-									<div class="text sign-up-text">Don't have an account? <label for="flip">Sigup now</label></div>
-								</div>
-							</form>
-						</div>
-						<div class="signup-form">
-							<div class="title">Signup</div>
-							<form onSubmit={registerSubmit}>
-								<div class="input-boxes">
-									<div class="input-box">
-										<i class="fas fa-user"></i>
-										<input type="text"
-											placeholder="Enter your name"
-											required
-											name='name'
-											value={userDetail.name}
-											onChange={changeUserDetail} />
-									</div>
-									<div class="input-box">
-										<i class="fas fa-envelope"></i>
-										<input type="email"
-											placeholder="Enter your email"
-											required
-											name='email'
-											value={userDetail.email}
-											onChange={changeUserDetail} />
-									</div>
-									<div class="input-box">
-										<i class="fas fa-lock"></i>
-										<input type="password"
-											placeholder="Enter your password"
-											required
-											name='password'
-											value={userDetail.password}
-											onChange={changeUserDetail}
-										/>
-									</div>
-									<div class="button input-box">
-										<input type="submit"
-											value="Register" />
-									</div>
-									<div class="text sign-up-text">Already have an account? <label for="flip">Login now</label></div>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+			}
+		</Fragment>
 	)
 }
 
